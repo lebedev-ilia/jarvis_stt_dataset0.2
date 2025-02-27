5b86f124fffb13235c598e31f053e2bfbefbd51c
lebedev-ilia <150586009+lebedev-ilia@users.noreply.github.com>
2025-02-27 19:01:41 +1000

    Add files via upload

---
+++ dataset/train.py
@@ -0,0 +1,35 @@
+import lightning.pytorch as pl
+from omegaconf import OmegaConf
+from nemo.collections.asr.models import EncDecHybridRNNTCTCBPEModel
+from nemo.core.config import hydra_runner
+from nemo.utils import logging
+from nemo.utils.exp_manager import exp_manager
+from nemo.utils.trainer_utils import resolve_trainer_cfg
+import logging
+
+logging.getLogger('nemo_logger').setLevel(logging.ERROR)
+logging.getLogger('nemo_logging').setLevel(logging.ERROR)
+logging.disable(logging.CRITICAL)
+
+@hydra_runner(
+    config_path="/Users/user/Desktop/exp/dataset/configs", config_name="fastconformer_hybrid_transducer_ctc_bpe"
+)
+def main(cfg):
+    logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg)}')
+
+    trainer = pl.Trainer(**resolve_trainer_cfg(cfg.trainer))
+    exp_manager(trainer, cfg.get("exp_manager", None))
+    asr_model = EncDecHybridRNNTCTCBPEModel(cfg=cfg.model, trainer=trainer)
+
+    # Initialize the weights of the model from another model, if provided via config
+    asr_model.maybe_init_from_pretrained_checkpoint(cfg)
+
+    trainer.fit(asr_model)
+
+    if hasattr(cfg.model, 'test_ds') and cfg.model.test_ds.manifest_filepath is not None:
+        if asr_model.prepare_test(trainer):
+            trainer.test(asr_model)
+
+
+if __name__ == '__main__':
+    main()  # noqa pylint: disable=no-value-for-parameter
